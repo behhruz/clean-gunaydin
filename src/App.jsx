c896f648e63a4c19eda041aa83c8052ee1971e1a
behhruz <bsatimbayev05@gmail.com>
2023-08-20 17:11:56 +0500

    done

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import "./App.css";
 import Header from "./components/Header/header";
 import Main from "./components/Main/main";
 
+
 function App() {
   return (
     <div className="wrapper">
       <Header />
-      <Main />
+       <Main />
     </div>
   );
 }
--- src/components/Main/main.jsx
+++ src/components/Main/main.jsx
@@ -1,8 +1,30 @@
 import React from "react";
+import "./main.css";
+import leftImg from "../../assets/logo/left.png";
+import rightImg from "../../assets/logo/right.png";
+import { useTranslation } from "react-i18next";
+import xolaImg from "../../assets/img/image 4.png";
+
 export const Main = () => {
- 
-  return <div className="main">
-  h
-  </div>;
+  const [t, i18n] = useTranslation("global");
+  return (
+    <div className="main">
+      <section className="first_section">
+        <img src={leftImg} alt="" />
+        <div className="first_section_child">
+          <div className="first_section_child_left">
+            <h1>{t("main.zagalovok")}</h1>
+            <p>{t("main.podzagalovok")}</p>
+            <button>{t("main.first-button")}</button>
+          </div>
+          <div className="first_section_child_right">
+            <img src={xolaImg} alt="" />
+            <div className="shadow">h</div>
+          </div>
+        </div>
+        <img src={rightImg} alt="" />
+      </section>
+    </div>
+  );
 };
 export default Main;
