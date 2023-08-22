eb381c8f72159595a3315ca8a4a6c95df6762fcf
behhruz <bsatimbayev05@gmail.com>
2023-08-22 13:36:43 +0500

    done

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -10,8 +10,19 @@ const Footer = () => {
   const [t, i18n] = useTranslation("global");
   return (
     <footer>
-      <div>
-        <img src={LogoFooter} alt="logo" className="logo-img"/>
+      <div className="footer-child">
+        <img src={LogoFooter} alt="logo" className="logo-img" />
+        <div className="logo-footer">
+          <a href="https://www.youtube.com/?hl=ru">
+            <img src={LogoYutub} alt="" />
+          </a>
+          <a href="https://www.instagram.com/">
+            <img src={LogoInstagram} alt="" />
+          </a>
+          <a href="https://web.telegram.org/a/">
+            <img src={LogoTelegram} alt="" />
+          </a>
+        </div>
       </div>
       <div className="active">
         <ul>
@@ -24,13 +35,13 @@ const Footer = () => {
       <div className="border">
         <ul>
           <div>
-          <li>{t("footer.telephone")}</li>
-          <li>{t("footer.phonenumber")}</li>
+            <li>{t("footer.telephone")}</li>
+            <li>{t("footer.phonenumber")}</li>
           </div>
-         <div>
-         <li>{t("footer.adres")}</li>
-          <li>{t("footer.adre2")}</li>
-         </div>
+          <div>
+            <li>{t("footer.adres")}</li>
+            <li>{t("footer.adre2")}</li>
+          </div>
         </ul>
       </div>
       <div className="footer">
@@ -46,7 +57,33 @@ const Footer = () => {
           </a>
         </div>
         <div>
-        <button className="btn-col-center"><a href="tel:+998333000057">{t("main.first-button")}</a></button>
+          <button className="btn-col-center">
+            <a href="tel:+998333000057">{t("main.first-button")}</a>
+          </button>
+        </div>
+      </div>
+      <div className="Responsive-wrapper">
+        <div className="Img_wrappers">
+          <img src={LogoFooter} alt="logo" className="logo-img" />
+          <div className="logo-footer">
+            <a href="https://www.youtube.com/?hl=ru">
+              <img src={LogoYutub} alt="" />
+            </a>
+            <a href="https://www.instagram.com/">
+              <img src={LogoInstagram} alt="" />
+            </a>
+            <a href="https://web.telegram.org/a/">
+              <img src={LogoTelegram} alt="" />
+            </a>
+          </div>
+        </div>
+        <div className="Img_wrappers">
+          <p className="phone_footer">{t("footer.telephone")}</p>
+          <p className="real-phone-number">{t("footer.phonenumber")}</p>
+        </div>
+        <div className="Img_wrappers">
+          <p className="phone_footer">{t("footer.adres")}</p>
+          <p className="real-phone-number">{t("footer.adre2")}</p>
         </div>
       </div>
     </footer>
